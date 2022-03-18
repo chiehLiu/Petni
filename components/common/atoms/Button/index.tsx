0540c7871e2d21c303c1ca4e616955a17e1fbd00
chieh <chieh@25sprout.com>
2022-03-19 00:28:51 +0800

    feat: working on SearchArea UI, not done

---
+++ components/common/atoms/Button/index.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react';
+import classnames from 'classnames';
+
+import styles from './index.module.scss';
+
+type ButtonProps = {
+  content: string;
+  className?: string;
+  onClick?: () => void;
+};
+
+const Button: FC<ButtonProps> = ({ className = '', content = '', onClick = () => {} }) => {
+  return (
+    <button
+      className={classnames( styles.button )}
+      onClick={onClick}
+    >
+      {content}
+    </button>
+  );
+};
+
+export default Button;
--- components/common/molecules/SearchArea/index.tsx
+++ components/common/molecules/SearchArea/index.tsx
@@ -1,22 +1,64 @@
 import SearchAreaTitle from '../../atoms/SearchAreaTitle';
+import Button from '../../atoms/Button';
+
+import styles from './index.module.scss';
 
 const SearchArea = () => {
   return (
-    <div>
+    <div className={styles.SearchArea}>
       <div>
         <SearchAreaTitle>我想尋找</SearchAreaTitle>
       </div>
-      <div></div>
+      <div>
+        <Button content={'123'}></Button>
+        <Button content={'123'}></Button>
+        <Button content={'123'}></Button>
+      </div>
       <div>
         <SearchAreaTitle>性別</SearchAreaTitle>
       </div>
-      <div></div>
+      <div>
+        <Button content={'123'}></Button>
+        <Button content={'123'}></Button>
+        <Button content={'123'}></Button>
+      </div>
       <div>
         <SearchAreaTitle>年齡</SearchAreaTitle>
       </div>
-      <div></div>
+      <div>
+        <Button content={'123'}></Button>
+        <Button content={'123'}></Button>
+        <Button content={'123'}></Button>
+      </div>
       <div>
         <SearchAreaTitle>顏色</SearchAreaTitle>
+      </div>
+      <div>
+        <Button content={'123'}></Button>
+        <Button content={'123'}></Button>
+        <Button content={'123'}></Button>
+        <Button content={'123'}></Button>
+        <Button content={'123'}></Button>
+        <Button content={'123'}></Button>
+        <Button content={'123'}></Button>
+        <Button content={'123'}></Button>
+      </div>
+      <div className={styles.ToggleBtn}>
+        <SearchAreaTitle>搜尋附近</SearchAreaTitle>
+        <label className={styles.switch}>
+          <input type="checkbox" />
+          <span className={`${styles.slider} ${styles.round}`}></span>
+        </label>
+      </div>
+      <div className={styles.ToggleBtn}>
+        <SearchAreaTitle>互動音效</SearchAreaTitle>
+        <label className={styles.switch}>
+          <input type="checkbox" />
+          <span className={`${styles.slider} ${styles.round}`}></span>
+        </label>
+      </div>
+      <div>
+        <Button content={'123'}></Button>
       </div>
     </div>
   );
